f9373f254b3465b784d5f4eec60077c3d74987c6
Pavel Krajcevski <pavel@cs.unc.edu>
2013-11-04 17:06:16 -0500

    Cleanup code

--- Graphics/Rendering/Lambency/Utils.hs
+++ Graphics/Rendering/Lambency/Utils.hs
@@ -13,23 +13,23 @@ import Data.Vect.Float.Util.Quaternion
 compareZero :: (DotProd v) => v -> Bool
 compareZero v = (abs $ v &. v) < 1e-6
 
+checkParallel :: (DotProd v) => v -> v -> Bool
+checkParallel v1 v2 = 1 - (abs $ v1 &. v2) < 1e-6 
+
 quatFromVecs :: Normal3 -> Normal3 -> UnitQuaternion
-quatFromVecs n1 n2 =
-  let cross = n1 &^ n2
-      cv = fromNormal cross
-      d = n1 &. n2
-  in
-   if 1 - (abs d) < 0.000001 then
-     if n1 &. n2 < 0 then
-       let n3X = toNormalUnsafe vec3X
-           n = n1 &^ n3X
-           nv = fromNormal n
-       in
-        if 1 - (abs $ n1 &. n3X) < 0.000001 then
-          rotU vec3Y pi
-        else
-          rotU nv pi
-      else
-       unitU
-   else
-     rotU cv $ angle (fromNormal n1) (fromNormal n2)
+quatFromVecs n1 n2
+  | 1 - (abs d) < 1e-6 = handleParallel
+  | otherwise = rotU cv $ angle (fromNormal n1) (fromNormal n2)
+  where
+    d = n1 &. n2
+    cv = fromNormal $ n1 &^ n2
+    handleParallel :: UnitQuaternion
+    handleParallel
+      | d < 0 = let n3X = toNormalUnsafe vec3X
+                    nv = fromNormal $ n1 &^ n3X
+                in
+                 if checkParallel n1 n3X then
+                   rotU vec3Y pi
+                 else
+                   rotU nv pi
+      | otherwise = unitU     
