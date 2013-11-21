a4fd09001e6475fde13e807c76f0fc04706ae0ed
Pavel Krajcevski <Krajcevski@gmail.com>
2013-11-21 00:15:42 -0500

    Add a plane mesh

--- Graphics/Rendering/Lambency/Mesh.hs
+++ Graphics/Rendering/Lambency/Mesh.hs
@@ -1,6 +1,7 @@
 module Graphics.Rendering.Lambency.Mesh (
   makeTriangle,
-  makeCube
+  makeCube,
+  makePlane
 ) where
 
 --------------------------------------------------------------------------------
@@ -71,16 +72,22 @@ makeCube = Mesh {
             replicate 4 (Vec3 (-1) 0 0)])
   -- Texture Coordinates
   ((concat . (replicate 6)) [Vec2 0 0, Vec2 1 0, Vec2 1 1, Vec2 0 1]),
-  
-  indices = [
-    0,  1,  2,      0,  2,  3,    -- front
-    4,  5,  6,      4,  6,  7,    -- back
-    8,  9,  10,     8,  10, 11,   -- top
-    12, 13, 14,     12, 14, 15,   -- bottom
-    16, 17, 18,     16, 18, 19,   -- right
-    20, 21, 22,     20, 22, 23    -- left
-  ]
+
+  indices = concat [[x, x+1, x+2, x, x+2, x+3] | x <- [0,4..20]]
 }
+
+makePlane :: Mesh
+makePlane = Mesh {
+  vertices = zipWith3 OTVertex3
+             [Vec3 x 0 z | z <- [(-1),(-0.9)..1], x <- [(-1),(-0.9)..1]]
+             (replicate (21*21) (Vec3 0 1 0))
+             [Vec2 u v | v <- [0,0.05..1], u <- [0,0.05..1]],
+  indices = concat [quadAt x y | y <- [0..19], x <- [0..19]]
+}
+  where quadAt x y =
+          [idxOf x y, idxOf x (y+1), idxOf (x+1) y,
+           idxOf (x+1) y, idxOf x (y+1), idxOf (x+1) (y+1)]
+        idxOf x y = y * 21 + x
 
 instance Renderable Mesh where
   createRenderObject m = createBasicRO (vertices m) (indices m)
