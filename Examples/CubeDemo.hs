2009ca8ac21ce8146b7bfad14047af6a2f771f0f
Pavel Krajcevski <Krajcevski@gmail.com>
2013-11-23 08:47:13 -0500

    Don't double load the material

--- Examples/CubeDemo.hs
+++ Examples/CubeDemo.hs
@@ -43,9 +43,8 @@ demoSVMap ro = Map.fromList [
       
     lu = LR.getMaterialVar (LR.material ro)
 
-planeObj :: IO (LR.GameObject CubeDemoObject)
-planeObj = do
-  mat <- LR.createSpotlightMaterial . Just =<< (LR.createSolidTexture (128, 128, 128, 255))
+planeObj :: LR.Material -> IO (LR.GameObject CubeDemoObject)
+planeObj mat = do
   ro <- LR.createRenderObject LR.makePlane mat
   return LR.GameObject {
     LR.renderObject = Just ro,
@@ -54,12 +53,10 @@ planeObj = do
     LR.update = \_ o _ -> Just o
   }
 
-cubeObj :: IO (LR.GameObject CubeDemoObject)
-cubeObj = do
-  ro <- (getDataFileName $ "crate" <.> "png") >>=
-        LR.loadTextureFromPNG >>=
-        LR.createSpotlightMaterial >>=
-        LR.createRenderObject LR.makeCube
+cubeObj :: LR.Material -> IO (LR.GameObject CubeDemoObject)
+cubeObj mat = do
+  (Just tex) <- getDataFileName ("crate" <.> "png") >>= LR.loadTextureFromPNG
+  ro <- LR.createRenderObject LR.makeCube (LR.switchTexture mat "diffuseTex" tex)
   return LR.GameObject {
     LR.renderObject = Just ro,
     LR.gameObject = DemoObject 1 zero $ rotU (Vec3 1 0 1) 0.6,
@@ -72,7 +69,8 @@ cubeObj = do
 main :: IO ()
 main = do
   m <- L.makeWindow 640 480 "Cube Demo"
-  objs <- sequence [cubeObj, planeObj]
+  mat <- LR.createSpotlightMaterial . Just =<< (LR.createSolidTexture (128, 128, 128, 255))
+  objs <- sequence [cubeObj mat, planeObj mat]
   case m of
     (Just win) -> L.run win (LR.GameCamera demoCam $ flip const) objs
     Nothing -> return ()
