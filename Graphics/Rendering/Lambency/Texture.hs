c2e7588c25f6e977b90a89144d966d98060b761b
Pavel Krajcevski <Krajcevski@gmail.com>
2013-11-22 23:03:25 -0500

    Add depth texture

--- Graphics/Rendering/Lambency/Texture.hs
+++ Graphics/Rendering/Lambency/Texture.hs
@@ -5,6 +5,7 @@ module Graphics.Rendering.Lambency.Texture (
   getHandle,
   createFramebufferObject,
   createSolidTexture,
+  createDepthTexture,
   loadTextureFromPNG
 ) where
 
@@ -13,18 +14,23 @@ import qualified Graphics.Rendering.OpenGL as GL
 
 import qualified Codec.Image.PNG as PNG
 
+import Graphics.Rendering.Lambency.Camera
+
 import Foreign.Ptr
 import Data.Array.Storable
 import Data.Word
 --------------------------------------------------------------------------------
 
+type FBOHandle = GL.FramebufferObject
 type TextureHandle = GL.TextureObject
 data TextureFormat = RGBA8
                    | RGB8
 data Texture = Texture TextureHandle TextureFormat
+             | RenderTexture TextureHandle FBOHandle Camera
 
 getHandle :: Texture -> TextureHandle
 getHandle (Texture h _) = h
+getHandle (RenderTexture h _ _) = h
 
 fmt2glpfmt :: TextureFormat -> GL.PixelFormat
 fmt2glpfmt RGBA8 = GL.RGBA
@@ -70,6 +76,28 @@ loadTextureFromPNG filename = do
           initializeTexture ptr (PNG.dimensions img) RGB8)
       
       return $ Just tex
+
+createDepthTexture :: Camera -> IO (Texture)
+createDepthTexture cam = do
+  handle <- GL.genObjectName
+  GL.activeTexture GL.$= GL.TextureUnit 0
+  GL.textureBinding GL.Texture2D GL.$= Just handle
+  GL.textureWrapMode GL.Texture2D GL.S GL.$= (GL.Repeated, GL.ClampToEdge)
+  GL.textureWrapMode GL.Texture2D GL.T GL.$= (GL.Repeated, GL.ClampToEdge)
+  GL.textureFilter GL.Texture2D GL.$= ((GL.Nearest, Nothing), GL.Nearest)
+  GL.depthTextureMode GL.Texture2D GL.$= GL.Intensity
+  GL.texImage2D GL.Texture2D GL.NoProxy 0 GL.DepthComponent32
+    (GL.TextureSize2D 512 512) 0 $ GL.PixelData GL.DepthComponent GL.UnsignedInt nullPtr
+
+  putStrLn "Creating framebuffer object..."
+  rbHandle <- GL.genObjectName
+  GL.bindFramebuffer GL.DrawFramebuffer GL.$= rbHandle
+  GL.framebufferTexture2D GL.DrawFramebuffer GL.DepthAttachment GL.Texture2D handle 0
+  status <- GL.get $ GL.framebufferStatus GL.DrawFramebuffer
+  putStrLn $ "Checking framebuffer status..." ++ (show status)
+  GL.bindFramebuffer GL.DrawFramebuffer GL.$= GL.defaultFramebufferObject
+
+  return $ RenderTexture handle rbHandle cam
 
 createSolidTexture :: (Word8, Word8, Word8, Word8) -> IO(Texture)
 createSolidTexture (r, g, b, a) = do
