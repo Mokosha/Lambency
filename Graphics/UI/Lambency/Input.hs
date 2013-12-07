5ddfb11026f2b94c4a19ab8b6d62536709cc1a6c
Pavel Krajcevski <Krajcevski@gmail.com>
2013-12-07 10:03:23 -0500

    Add key callback and use safer modification functions

--- Graphics/UI/Lambency/Input.hs
+++ Graphics/UI/Lambency/Input.hs
@@ -10,7 +10,10 @@ module Graphics.UI.Lambency.Input (
 
 import qualified Graphics.UI.GLFW as GLFW
 
+import Control.Monad (liftM)
 import Control.Concurrent
+
+import qualified Data.Set as Set
 
 --------------------------------------------------------------------------------
 
@@ -18,43 +21,49 @@ data MiscInput = Scroll Double Double
                | Resize
 
 data Input = Input {
-  keysPressed :: [GLFW.Key],
-  keysReleased :: [GLFW.Key],
+  keysPressed :: Set.Set GLFW.Key,
   mbPressed :: [Int],
   cursor :: Maybe (Float, Float),
   misc :: [MiscInput]
 }
 
 kEmptyInput :: Input
-kEmptyInput = Input {
-  keysPressed = [], keysReleased = [], mbPressed = [], cursor = Nothing, misc = []
-}
+kEmptyInput = Input { keysPressed = Set.empty, mbPressed = [], cursor = Nothing, misc = [] }
 
 type InputControl = MVar Input
 
--- Returns a snapshot of the input and resets it
+-- Returns a snapshot of the input
 getInput :: InputControl -> IO(Input)
-getInput input = do
-  ipt <- takeMVar input
-  putMVar input ipt
-  return ipt
+getInput = readMVar
 
 scrollCallback :: InputControl -> GLFW.Window -> Double -> Double -> IO ()
-scrollCallback ctl _ xoff yoff = do
-  putStrLn "Scrollin!"
-  input <- takeMVar ctl
-  putMVar ctl (updateScroll input)
+scrollCallback ctl _ xoff yoff = modifyMVar_ ctl (liftM updateScroll . return)
   where
     updateScroll :: Input -> Input
     updateScroll input =
-      (\ipt -> ipt { misc = (Scroll xoff yoff) : (filter isScroll $ misc input) }) input
+      (\ipt -> ipt { misc = (Scroll xoff yoff) : (filter notScroll $ misc input) }) input
 
-    isScroll :: MiscInput -> Bool
-    isScroll (Scroll _ _) = True
-    isScroll _ = False
+    notScroll :: MiscInput -> Bool
+    notScroll (Scroll _ _) = False
+    notScroll _ = True
+
+keyCallback :: InputControl -> GLFW.Window ->
+               GLFW.Key -> Int -> GLFW.KeyState -> GLFW.ModifierKeys ->
+               IO ()
+keyCallback ctl _ key _ keystate _ = modifyMVar_ ctl (liftM modifyKeys . return)
+  where
+    updateKeys :: (Set.Set GLFW.Key -> Set.Set GLFW.Key) -> Input -> Input
+    updateKeys fn = (\input -> input { keysPressed = fn (keysPressed input) })
+
+    modifyKeys :: Input -> Input
+    modifyKeys = case keystate of
+      GLFW.KeyState'Pressed -> updateKeys $ Set.insert key
+      GLFW.KeyState'Released -> updateKeys $ Set.delete key
+      _ -> id
 
 mkInputControl :: GLFW.Window -> IO (InputControl)
 mkInputControl win = do
   ctl <- newMVar kEmptyInput
   GLFW.setScrollCallback win (Just $ scrollCallback ctl)
+  GLFW.setKeyCallback win (Just $ keyCallback ctl)
   return ctl
