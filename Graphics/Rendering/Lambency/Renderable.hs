9e474db713b8eabc63184b3c544bf7e1a59d9fbc
Pavel Krajcevski <pavel@cs.unc.edu>
2014-04-30 17:22:41 -0400

    Small refactoring

--- Graphics/Rendering/Lambency/Renderable.hs
+++ Graphics/Rendering/Lambency/Renderable.hs
@@ -23,49 +23,51 @@ import Foreign.Ptr
 clearBuffers :: IO ()
 clearBuffers = GL.clear [GL.ColorBuffer, GL.DepthBuffer]
 
+ptrsize :: (Storable a) => [a] -> GL.GLsizeiptr
+ptrsize [] = toEnum 0
+ptrsize xs = toEnum $ length xs * (sizeOf $ head xs)
+
+setupBuffer :: (Storable a) => GL.BufferTarget -> [a] -> IO ( GL.BufferObject )
+setupBuffer tgt xs = do
+  buf <- GL.genObjectName
+  GL.bindBuffer tgt GL.$= (Just buf)
+  varr <- newListArray (0, length xs - 1) xs
+  withStorableArray varr (\ptr -> GL.bufferData tgt GL.$= (ptrsize xs, ptr, GL.StaticDraw))
+  return buf
+
 createBasicRO :: [Vertex] -> [Int16] -> Material -> IO (RenderObject)
+
+-- If there's no vertices, then there's nothing to render...
 createBasicRO [] _ _ = do
   return $ RenderObject {
     material = Map.empty,
     render = \_ _ -> return ()
   }
+
 createBasicRO (v:vs) idxs mat =
   let
     flts :: [Float]
     flts = (v:vs) >>= toFloats
-  in do
-    vbo <- setupBuffer GL.ArrayBuffer flts
-    ibo <- setupBuffer GL.ElementArrayBuffer idxs
-    return $ RenderObject {
-      material = mat,
-      render = createRenderFunc vbo ibo $ fromIntegral (length idxs)
-    }
-  where
-    ptrsize :: (Storable a) => [a] -> GL.GLsizeiptr
-    ptrsize [] = toEnum 0
-    ptrsize xs = toEnum $ length xs * (sizeOf $ head xs)
 
-    setupBuffer :: (Storable a) => GL.BufferTarget -> [a] -> IO ( GL.BufferObject )
-    setupBuffer tgt xs = do
-      buf <- GL.genObjectName
-      GL.bindBuffer tgt GL.$= (Just buf)
-      varr <- newListArray (0, length xs - 1) xs
-      withStorableArray varr (\ptr -> GL.bufferData tgt GL.$= (ptrsize xs, ptr, GL.StaticDraw))
-      return buf
+    bindShaderVertexAttributes :: Shader -> IO ()
+    bindShaderVertexAttributes shdr = let
 
-    bindShader :: Shader -> IO ()
-    bindShader shdr = do
-      mapM_ (\(loc, desc) -> GL.vertexAttribPointer loc GL.$= (GL.ToFloat, desc)) $
-        zip (map lu $ getAttribNames v) (getDescriptors v)
-     where
-       lu :: String -> GL.AttribLocation
-       lu name = let svs = getShaderVars shdr
-          in case Map.lookup name svs of
-            Nothing -> GL.AttribLocation (-1)
-            Just var -> case var of
-              Uniform _ _ -> GL.AttribLocation (-1)
-              Attribute _ loc -> loc
+      -- Lookup the location for the given attribute name in the shader program
+      lu :: String -> GL.AttribLocation
+      lu name = let
+        svs = getShaderVars shdr
+        in
+         case Map.lookup name svs of
+           Nothing -> GL.AttribLocation (-1)
+           Just var -> case var of
+             Uniform _ _ -> GL.AttribLocation (-1)
+             Attribute _ loc -> loc
+      in do
+        mapM_ (\(loc, desc) -> GL.vertexAttribPointer loc GL.$= (GL.ToFloat, desc)) $
+          zip (map lu $ getAttribNames v) (getDescriptors v)
 
+    -- Takes as input an array of vertices and indices and returns a function
+    -- that renders the vertices for a given shader and shader variable mapping
     createRenderFunc :: GL.BufferObject -> GL.BufferObject ->
                         GL.NumArrayIndices -> (Shader -> ShaderMap -> IO ())
     createRenderFunc vbo ibo nIndices = (\shdr shdrmap -> do
@@ -78,12 +80,20 @@ createBasicRO (v:vs) idxs mat =
 
         -- Bind appropriate buffers
         GL.bindBuffer GL.ArrayBuffer GL.$= Just vbo
-        bindShader shdr
+        bindShaderVertexAttributes shdr
 
         GL.bindBuffer GL.ElementArrayBuffer GL.$= Just ibo
 
         -- Render
         GL.drawElements GL.Triangles nIndices GL.UnsignedShort nullPtr)
+
+  in do
+    vbo <- setupBuffer GL.ArrayBuffer flts
+    ibo <- setupBuffer GL.ElementArrayBuffer idxs
+    return $ RenderObject {
+      material = mat,
+      render = createRenderFunc vbo ibo $ fromIntegral (length idxs)
+    }
 
 class Renderable a where
   createRenderObject :: a -> Material -> IO (RenderObject)
