8b883aec705c621234a4999ab2c424e0cf85c8be
Pavel Krajcevski <Krajcevski@gmail.com>
2018-04-30 17:12:52 -0700

    Move game loop into its own module so that multiple entry points can call it.

--- lib/Lambency.hs
+++ lib/Lambency.hs
@@ -44,23 +44,18 @@ module Lambency (
 import Prelude hiding ((.))
 
 import Control.Monad.RWS.Strict
-import Control.Monad.Reader
-import Control.Monad.State
 import Control.Wire ((.))
 import qualified Control.Wire as W
-
-import Data.Time
 
 import GHC.Generics (Generic)
 
 import qualified Graphics.UI.GLFW as GLFW
 import qualified Graphics.Rendering.OpenGL as GL
 
-import FRP.Netwire.Input.GLFW
-
 import Lambency.Bounds
 import Lambency.Camera
 import Lambency.Font
+import Lambency.GameLoop
 import Lambency.GameObject
 import Lambency.GameSession
 import Lambency.Light
@@ -68,7 +63,6 @@ import Lambency.Loaders
 import Lambency.Material
 import Lambency.Mesh
 import Lambency.Renderer
-import Lambency.ResourceLoader
 import Lambency.Shader
 import Lambency.Sound
 import Lambency.Sprite
@@ -78,9 +72,6 @@ import Lambency.Types
 import Lambency.UI
 import Lambency.Utils
 
-import Linear
-
-import System.CPUTime
 import System.IO
 --------------------------------------------------------------------------------
 
@@ -164,152 +155,6 @@ withWindow width height title f = do
   destroyWindow mwin
   return x
 
-maximumFramerate :: NominalDiffTime
-maximumFramerate = fromRational . toRational $ (1.0 / 10.0 :: Double)
-
--- When we handle actions, only really print logs and play any sounds
--- that may need to start or stop.
-handleAction :: OutputAction -> IO ()
-handleAction (SoundAction sound cmd) = handleCommand sound cmd
-handleAction (LogAction s) = putStrLn s
-handleAction (WireframeAction True) = GL.polygonMode GL.$= (GL.Line, GL.Line)
-handleAction (WireframeAction False) = GL.polygonMode GL.$= (GL.Fill, GL.Fill)
-
-step :: a -> Game a -> TimeStep ->
-        GameMonad (Maybe a, Camera, [Light], Game a)
-step go game t = do
-  (Right cam, nCamWire) <-
-    W.stepWire (getContinuousWire $ mainCamera game) t (Right ())
-  (lights, lwires) <-
-    collect <$>
-    mapM (\w -> W.stepWire w t $ Right ()) (getContinuousWire <$> dynamicLights game)
-  (Right result, gameWire) <-
-    W.stepWire (getContinuousWire $ gameLogic game) t (Right go)
-  case result of
-    Nothing -> return (result, cam, lights, newGame nCamWire lwires W.mkEmpty)
-    Just x  -> return (x `seq` result, cam, lights, newGame nCamWire lwires gameWire)
-  where
-    collect :: [(Either e b, GameWire a b)] -> ([b], [GameWire a b])
-    collect [] = ([], [])
-    collect ((Left _, _) : _) = error "Internal -- Light wire inhibited?"
-    collect ((Right obj, wire) : rest) = (obj : objs, wire : wires)
-      where
-        (objs, wires) = collect rest
-
-    newGame cam lights logic =
-      Game { mainCamera = CW cam
-           , dynamicLights = CW <$> lights
-           , gameLogic = CW logic
-           }
-
-data GameLoopConfig = GameLoopConfig {
-  gameRenderer :: Renderer,
-  simpleQuadSprite :: Sprite,
-  glfwInputControl :: Maybe GLFWInputControl,
-  windowDimensions :: V2 Int
-}
-
-data GameLoopState a = GameLoopState {
-  currentGameValue :: a,
-  currentGameLogic :: Game a,
-  currentGameSession :: GameSession,
-  currentPhysicsAccum :: NominalDiffTime,
-  lastFramePicoseconds :: Integer
-}
-
-type GameLoopM a = ReaderT GameLoopConfig (StateT (GameLoopState a) IO)
-
-runLoop :: UTCTime -> GameLoopM a ()
-runLoop prevFrameTime = do
-  (GameLoopState _ _ _ accumulator _) <- get
-  -- Step
-  thisFrameTime <- liftIO getCurrentTime
-  let newAccum = accumulator + (diffUTCTime thisFrameTime prevFrameTime)
-  modify $ \ls -> ls { currentPhysicsAccum = min newAccum maximumFramerate }
-  (go, (nextsession, accum), nextGame) <- stepGame
-
-  case go of
-    Just gobj -> do
-      ls <- get
-      put $ GameLoopState gobj nextGame nextsession accum (lastFramePicoseconds ls)
-      runLoop thisFrameTime
-    Nothing -> return ()
-
-type TimeStepper = (GameSession, NominalDiffTime)
-
-stepGame :: GameLoopM a (Maybe a, TimeStepper, Game a)
-stepGame = do
-  (GameLoopState go game session accum _) <- get
-  if (accum < physicsDeltaUTC)
-    then return (Just go, (session, accum), game)
-    else runGame
-
-runGame :: GameLoopM a (Maybe a, TimeStepper, Game a)
-runGame = do
-  gameLoopConfig <- ask
-  gls <- get
-  (hasInput, ipt) <- liftIO $ case glfwInputControl gameLoopConfig of
-    Just glfwIpt -> getInput glfwIpt >>= (\x -> return (True, x))
-    Nothing -> return (False, emptyGLFWState)
-
-  -- Retreive the next time step from our game session
-  (ts, nextSess) <- liftIO $ W.stepSession (currentGameSession gls)
-
-  let
-    -- The game step is the complete GameMonad computation that
-    -- produces four values: Either inhibition or a new game value
-    -- A new camera, a list of dynamic lights, and the next simulation
-    -- wire
-    gameStep = step (currentGameValue gls) (currentGameLogic gls) ts
-
-    -- We need to render if we're going to fall below the physics threshold
-    -- on the next frame. This simulates a while loop. If we don't fall
-    -- through this threshold, we will perform another physics step before
-    -- we finally decide to render.
-    accum = currentPhysicsAccum gls
-
-    winDims = windowDimensions gameLoopConfig
-    renderTime = lastFramePicoseconds gls
-    sprite = simpleQuadSprite gameLoopConfig
-    frameConfig = GameConfig (gameRenderer gameLoopConfig) renderTime winDims sprite
-
-  -- This is the meat of the step routine. This calls runRWS on the
-  -- main game wire, and uses the results to figure out what needs
-  -- to be done.
-  ((result, cam, lights, nextGame), newIpt, (actions, renderActs)) <-
-      liftIO $ runRWST (nextFrame gameStep) frameConfig ipt
-
-  -- The ReaderT RenderConfig IO program that will do the actual rendering
-  let accumRemainder = accum - physicsDeltaUTC
-      needsRender = case result of
-        Nothing -> False
-        Just _ -> accumRemainder < physicsDeltaUTC
-
-  frameTime <-
-    if needsRender
-    then liftIO $ do
-      t <- getCPUTime
-      render (gameRenderer gameLoopConfig) lights cam renderActs
-      t' <- getCPUTime
-      return (t' - t)
-    else return renderTime
-
-  _ <- liftIO $ do
-    -- Actually do the associated actions
-    mapM_ handleAction actions
-
-    -- Poll the input
-    when hasInput $ case glfwInputControl gameLoopConfig of
-      Just glfwIpt -> pollGLFW newIpt glfwIpt >> return ()
-      Nothing -> return ()
-
-  -- If our main wire inhibited, return immediately.
-  case result of
-    Just obj -> do
-      put $ GameLoopState obj nextGame nextSess accumRemainder frameTime
-      stepGame
-    Nothing -> return (result, (nextSess, accum), nextGame)
-
 -- TODO: This function uses a  hacky method of quitting the game currently
 -- which doesn't release all of the resources associated with the game logic.
 -- As such, care should be taken to make sure that you're not actually trying to
@@ -322,28 +167,13 @@ runWithGLFW win r initialGameObject (Game cam lights (CW logic)) = do
   hSetBuffering stdout NoBuffering
 
   GLFW.swapInterval 1
-  ictl <- mkInputControl win
-  curTime <- getCurrentTime
 
   -- Stick in an initial poll events call...
   GLFW.pollEvents
 
-  -- !FIXME! Use fully opaque 'mask' texture that we can change the color and
-  -- size for dynamically. This isn't the best way to do this, but it'll work.
-  (sprite, unloadSprite) <-
-    runResourceLoader r $ createSolidTexture (pure 255)
-                      >>= loadStaticSpriteWithMask
+  (config, unloadSprite) <- mkLoopConfig r (Just win)
 
-  -- Collect the window dimensions. TODO: This should be done every frame so
-  -- that we can properly update our UI on state changes. For now, we just tell
-  -- GLFW to prevent the user from resizing the window, but that need not be a
-  -- restriction.
-  winDims <- uncurry V2 <$> liftIO (GLFW.getWindowSize win)
-
-  let statePrg = runReaderT (runLoop curTime)
-               $ GameLoopConfig r sprite (Just ictl) winDims
-
-      -- Amend the game logic such that it actually quits if we hit the little
+  let -- Amend the game logic such that it actually quits if we hit the little
       -- x in the corner of the window.
       quitLogic = let mkQuitter w = W.mkGen $ \dt x -> do
                         needsQuit <- GameMonad
@@ -354,14 +184,9 @@ runWithGLFW win r initialGameObject (Game cam lights (CW logic)) = do
                           else return (res, mkQuitter w')
                   in CW $ mkQuitter logic
 
-  evalStateT statePrg $
-    GameLoopState
-    { currentGameValue = initialGameObject
-    , currentGameLogic = Game cam lights quitLogic
-    , currentGameSession = mkGameSession
-    , currentPhysicsAccum = toEnum 0
-    , lastFramePicoseconds = 0
-    }
+      loopState = mkLoopState initialGameObject $ Game cam lights quitLogic
+
+  runGameLoop loopState config
 
   unloadSprite
   hSetBuffering stdout oldBuffering
---
+++ lib/Lambency/GameLoop.hs
@@ -0,0 +1,211 @@
+module Lambency.GameLoop (
+  GameLoopState, mkLoopState,
+  GameLoopConfig, mkLoopConfig,
+  runGameLoop
+  ) where
+
+--------------------------------------------------------------------------------
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.RWS.Strict
+import qualified Control.Wire as W
+
+import qualified Graphics.UI.GLFW as GLFW
+import qualified Graphics.Rendering.OpenGL as GL
+import Data.Time
+
+import FRP.Netwire.Input.GLFW
+
+import Lambency.ResourceLoader
+import Lambency.Sound
+import Lambency.Sprite
+import Lambency.Texture
+import Lambency.Types
+import Lambency.GameSession
+
+import System.CPUTime
+
+import Linear
+--------------------------------------------------------------------------------
+
+
+maximumFramerate :: NominalDiffTime
+maximumFramerate = fromRational . toRational $ (1.0 / 10.0 :: Double)
+
+-- When we handle actions, only really print logs and play any sounds
+-- that may need to start or stop.
+handleAction :: OutputAction -> IO ()
+handleAction (SoundAction sound cmd) = handleCommand sound cmd
+handleAction (LogAction s) = putStrLn s
+handleAction (WireframeAction True) = GL.polygonMode GL.$= (GL.Line, GL.Line)
+handleAction (WireframeAction False) = GL.polygonMode GL.$= (GL.Fill, GL.Fill)
+
+step :: a -> Game a -> TimeStep ->
+        GameMonad (Maybe a, Camera, [Light], Game a)
+step go game t = do
+  (Right cam, nCamWire) <-
+    W.stepWire (getContinuousWire $ mainCamera game) t (Right ())
+  (lights, lwires) <-
+    collect <$>
+    mapM (\w -> W.stepWire w t $ Right ()) (getContinuousWire <$> dynamicLights game)
+  (Right result, gameWire) <-
+    W.stepWire (getContinuousWire $ gameLogic game) t (Right go)
+  case result of
+    Nothing -> return (result, cam, lights, newGame nCamWire lwires W.mkEmpty)
+    Just x  -> return (x `seq` result, cam, lights, newGame nCamWire lwires gameWire)
+  where
+    collect :: [(Either e b, GameWire a b)] -> ([b], [GameWire a b])
+    collect [] = ([], [])
+    collect ((Left _, _) : _) = error "Internal -- Light wire inhibited?"
+    collect ((Right obj, wire) : rest) = (obj : objs, wire : wires)
+      where
+        (objs, wires) = collect rest
+
+    newGame cam lights logic =
+      Game { mainCamera = CW cam
+           , dynamicLights = CW <$> lights
+           , gameLogic = CW logic
+           }
+
+data GameLoopConfig = GameLoopConfig {
+  gameRenderer :: Renderer,
+  simpleQuadSprite :: Sprite,
+  glfwInputControl :: Maybe GLFWInputControl,
+  windowDimensions :: V2 Int
+}
+
+mkLoopConfig :: Renderer -> Maybe GLFW.Window -> IO (GameLoopConfig, IO ())
+mkLoopConfig r win' = do
+  -- !FIXME! Use fully opaque 'mask' texture that we can change the color and
+  -- size for dynamically. This isn't the best way to do this, but it'll work.
+  (sprite, unloadSprite) <-
+    runResourceLoader r $ createSolidTexture (pure 255)
+                      >>= loadStaticSpriteWithMask
+
+  -- Collect the window dimensions. TODO: This should be done every frame so
+  -- that we can properly update our UI on state changes. For now, we just tell
+  -- GLFW to prevent the user from resizing the window, but that need not be a
+  -- restriction.
+  (winDims, ictl) <- case win' of
+    Just win -> do
+      winDims <- uncurry V2 <$> liftIO (GLFW.getWindowSize win)
+      ictl <- Just <$> mkInputControl win
+      return (winDims, ictl)
+    Nothing -> return (V2 0 0, Nothing)
+
+  return (GameLoopConfig r sprite ictl winDims, unloadSprite)
+
+data GameLoopState a = GameLoopState {
+  currentGameValue :: a,
+  currentGameLogic :: Game a,
+  currentGameSession :: GameSession,
+  currentPhysicsAccum :: NominalDiffTime,
+  lastFramePicoseconds :: Integer
+}
+
+mkLoopState :: a -> Game a -> GameLoopState a
+mkLoopState initialVal initGame = GameLoopState
+    { currentGameValue = initialVal
+    , currentGameLogic = initGame
+    , currentGameSession = mkGameSession
+    , currentPhysicsAccum = toEnum 0
+    , lastFramePicoseconds = 0
+    }
+
+type GameLoopM a = ReaderT GameLoopConfig (StateT (GameLoopState a) IO)
+
+runLoop :: UTCTime -> GameLoopM a ()
+runLoop prevFrameTime = do
+  (GameLoopState _ _ _ accumulator _) <- get
+  -- Step
+  thisFrameTime <- liftIO getCurrentTime
+  let newAccum = accumulator + (diffUTCTime thisFrameTime prevFrameTime)
+  modify $ \ls -> ls { currentPhysicsAccum = min newAccum maximumFramerate }
+  (go, (nextsession, accum), nextGame) <- stepGame
+
+  case go of
+    Just gobj -> do
+      ls <- get
+      put $ GameLoopState gobj nextGame nextsession accum (lastFramePicoseconds ls)
+      runLoop thisFrameTime
+    Nothing -> return ()
+
+runGameLoop :: GameLoopState a -> GameLoopConfig -> IO ()
+runGameLoop st config = do
+  curTime <- getCurrentTime
+  evalStateT (runReaderT (runLoop curTime) config) st
+
+type TimeStepper = (GameSession, NominalDiffTime)
+
+stepGame :: GameLoopM a (Maybe a, TimeStepper, Game a)
+stepGame = do
+  (GameLoopState go game session accum _) <- get
+  if (accum < physicsDeltaUTC)
+    then return (Just go, (session, accum), game)
+    else runGame
+
+runGame :: GameLoopM a (Maybe a, TimeStepper, Game a)
+runGame = do
+  gameLoopConfig <- ask
+  gls <- get
+  (hasInput, ipt) <- liftIO $ case glfwInputControl gameLoopConfig of
+    Just glfwIpt -> getInput glfwIpt >>= (\x -> return (True, x))
+    Nothing -> return (False, emptyGLFWState)
+
+  -- Retreive the next time step from our game session
+  (ts, nextSess) <- liftIO $ W.stepSession (currentGameSession gls)
+
+  let
+    -- The game step is the complete GameMonad computation that
+    -- produces four values: Either inhibition or a new game value
+    -- A new camera, a list of dynamic lights, and the next simulation
+    -- wire
+    gameStep = step (currentGameValue gls) (currentGameLogic gls) ts
+
+    -- We need to render if we're going to fall below the physics threshold
+    -- on the next frame. This simulates a while loop. If we don't fall
+    -- through this threshold, we will perform another physics step before
+    -- we finally decide to render.
+    accum = currentPhysicsAccum gls
+
+    winDims = windowDimensions gameLoopConfig
+    renderTime = lastFramePicoseconds gls
+    sprite = simpleQuadSprite gameLoopConfig
+    frameConfig = GameConfig (gameRenderer gameLoopConfig) renderTime winDims sprite
+
+  -- This is the meat of the step routine. This calls runRWS on the
+  -- main game wire, and uses the results to figure out what needs
+  -- to be done.
+  ((result, cam, lights, nextGame), newIpt, (actions, renderActs)) <-
+      liftIO $ runRWST (nextFrame gameStep) frameConfig ipt
+
+  -- The ReaderT RenderConfig IO program that will do the actual rendering
+  let accumRemainder = accum - physicsDeltaUTC
+      needsRender = case result of
+        Nothing -> False
+        Just _ -> accumRemainder < physicsDeltaUTC
+
+  frameTime <-
+    if needsRender
+    then liftIO $ do
+      t <- getCPUTime
+      render (gameRenderer gameLoopConfig) lights cam renderActs
+      t' <- getCPUTime
+      return (t' - t)
+    else return renderTime
+
+  _ <- liftIO $ do
+    -- Actually do the associated actions
+    mapM_ handleAction actions
+
+    -- Poll the input
+    when hasInput $ case glfwInputControl gameLoopConfig of
+      Just glfwIpt -> pollGLFW newIpt glfwIpt >> return ()
+      Nothing -> return ()
+
+  -- If our main wire inhibited, return immediately.
+  case result of
+    Just obj -> do
+      put $ GameLoopState obj nextGame nextSess accumRemainder frameTime
+      stepGame
+    Nothing -> return (result, (nextSess, accum), nextGame)
